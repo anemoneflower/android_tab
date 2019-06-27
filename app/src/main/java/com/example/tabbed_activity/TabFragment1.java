2ffcbd133b29d7744a6f241dbfe7d6d1e9ce4ed9
seoyun <sallylucky@naver.com>
2019-06-27 10:57:52 +0900

    fail-permission

--- app/src/main/java/com/example/tabbed_activity/TabFragment1.java
+++ app/src/main/java/com/example/tabbed_activity/TabFragment1.java
@@ -1,20 +1,37 @@
 package com.example.tabbed_activity;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class TabFragment1 extends Fragment {
+
+
+
     private RecyclerView mRecyclerView;
     private RecyclerImageTextAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
@@ -46,10 +63,18 @@ public class TabFragment1 extends Fragment {
     }
 
     private void initDataset() {
-        mMyData = new ArrayList<ContactRecyclerItem>();
-        addContact(getResources().getDrawable(R.drawable.default_icon), "Kim", "01086731742") ;
-        addContact(getResources().getDrawable(R.drawable.default_icon), "Son", "01085467389") ;
-        addContact(getResources().getDrawable(R.drawable.default_icon), "Jane", "01046885813") ;
+        mMyData = getContactList();
+        ContactRecyclerItem contactItem;
+
+        for (int i=0; i<mMyData.size(); i++){
+            contactItem = mMyData.get(i);
+            Drawable drawable = new BitmapDrawable(getResources(), loadContactPhoto(getActivity().getContentResolver(), contactItem.getPersonID(), contactItem.getIconID()));
+            contactItem.setIcon(drawable);
+        }
+                //new ArrayList<ContactRecyclerItem>();
+//        addContact(getResources().getDrawable(R.drawable.default_icon), "Kim", "01086731742") ;
+//        addContact(getResources().getDrawable(R.drawable.default_icon), "Son", "01085467389") ;
+//        addContact(getResources().getDrawable(R.drawable.default_icon), "Jane", "01046885813") ;
 
     }
 
@@ -63,4 +88,91 @@ public class TabFragment1 extends Fragment {
 
         mMyData.add(item);
     }
+
+    public ArrayList<ContactRecyclerItem> getContactList(){
+
+
+
+
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String[] projection = new String[]{
+                ContactsContract.CommonDataKinds.Phone.NUMBER,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.Contacts.PHOTO_ID,
+                ContactsContract.Contacts._ID
+        };
+//        String[] selectionArgs = null;
+        String sortOrder = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
+        Cursor cursor = getActivity().getContentResolver().query(uri, projection, null, null, sortOrder);
+//        LinkedHashSet<ContactRecyclerItem> hashlist = new LinkedHashSet<>();
+        ArrayList<ContactRecyclerItem> contactItems = new ArrayList<>();
+        if(cursor.moveToFirst()){
+            do{
+                long photo_id = cursor.getLong(2);
+                long person_id = cursor.getLong(3);
+                ContactRecyclerItem contactItem = new ContactRecyclerItem();
+                contactItem.setName(cursor.getString(1));
+                contactItem.setPhone(cursor.getString(0));
+                contactItem.setIconID(photo_id);
+                contactItem.setPersonID(person_id);
+
+                contactItems.add(contactItem);
+//                hashlist.add(contactItem);
+            }while (cursor.moveToNext());
+        }
+        return contactItems;
+    }
+    public Bitmap loadContactPhoto(ContentResolver cr, long id, long photo_id){
+        Uri uri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, id);
+        InputStream input = ContactsContract.Contacts.openContactPhotoInputStream(cr, uri);
+        if(input != null)
+            return resizingBitmap(BitmapFactory.decodeStream(input));
+        else
+            Log.d("PHOTO", "first try failed to load photo");
+
+        byte[] photoBytes = null;
+        Uri photoUri = ContentUris.withAppendedId(ContactsContract.Data.CONTENT_URI, photo_id);
+        Cursor c = cr.query(photoUri, new String[]{ContactsContract.CommonDataKinds.Photo.PHOTO}, null, null,null);
+        try {
+            if (c.moveToFirst())
+                photoBytes = c.getBlob(0);
+        } catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            c.close();
+        }
+
+        if(photoBytes != null)
+            return resizingBitmap(BitmapFactory.decodeByteArray(photoBytes, 0, photoBytes.length));
+        else
+            Log.d("PHOTO", "second try also failed");
+        return null;
+    }
+
+    public Bitmap resizingBitmap(Bitmap oBitmap){
+        if(oBitmap==null)
+            return null;
+        float width = oBitmap.getWidth();
+        float height = oBitmap.getHeight();
+        float resizing_size = 120;
+        Bitmap rBitmap = null;
+        if (width > resizing_size){
+            float mWidth = (float)(width/100);
+            float fScale = (float)(resizing_size/mWidth);
+            width *= (fScale/100);
+            height *= (fScale/100);
+        }else if (height>resizing_size){
+            float mHeight = (float) (height/100);
+            float fScale = (float)(resizing_size/mHeight);
+            width *= (fScale/100);
+            height *= (fScale/100);
+        }
+
+        rBitmap = Bitmap.createScaledBitmap(oBitmap, (int)width, (int)height, true);
+        return rBitmap;
+    }
+
+
+
+
 }
