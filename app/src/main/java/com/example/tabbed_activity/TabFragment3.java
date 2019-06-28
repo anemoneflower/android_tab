545cca779ed0c64fb504edc74fa09f8ee29c7a2f
김선규 <sklationd@gmail.com>
2019-06-29 01:03:38 +0900

    floating widget
    app list 받아서 spinner로 출력

--- app/src/main/java/com/example/tabbed_activity/TabFragment3.java
+++ app/src/main/java/com/example/tabbed_activity/TabFragment3.java
@@ -14,6 +14,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -24,14 +27,60 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.futuremind.recyclerviewfastscroll.FastScroller;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TabFragment3 extends Fragment {
     private static final int SYSTEM_ALERT_WINDOW_PERMISSION = 2084;
     View view;
+    public ArrayList<String> appNameList;
+    ArrayAdapter<String> arrayAdapter;
+    private Spinner spinner0;
+    List<ResolveInfo> pkgAppsList;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_3, container, false);
+
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        pkgAppsList = getContext().getPackageManager().queryIntentActivities( mainIntent, 0);
+        PackageManager pm = getContext().getPackageManager();
+
+        appNameList = new ArrayList<>();
+
+        for (ResolveInfo resolveInfo : pkgAppsList) {
+            //package name
+            Log.d("DEBUG DEBUG DEBUG", "Installed package :" + resolveInfo.activityInfo.packageName);
+
+            try {
+                String appname = pm.getApplicationLabel(pm.getApplicationInfo(resolveInfo.activityInfo.packageName, PackageManager.GET_META_DATA)).toString();
+                Log.d("DEBUGDEBUGDEBUG",appname);
+                appNameList.add(appname);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        arrayAdapter = new ArrayAdapter<>(getContext(),
+                android.R.layout.simple_spinner_dropdown_item,
+               appNameList);
+
+        spinner0 = (Spinner) view.findViewById(R.id.spinner0);
+        spinner0.setAdapter(arrayAdapter);
+
+        spinner0.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getContext(),appNameList.get(i)+"가 선택되었습니다.",
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+
         view.findViewById(R.id.overlay_on_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,41 +97,6 @@ public class TabFragment3 extends Fragment {
                 else {
                     askPermission();
                     Toast.makeText(getActivity(), "You need System Alert Window Permission to do this", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        view.findViewById(R.id.setting_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-                mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-                List<ResolveInfo> pkgAppsList = getContext().getPackageManager().queryIntentActivities( mainIntent, 0);
-//                try
-//                {
-//                    Drawable icon = getActivity().getPackageManager().getApplicationIcon("com.example.testnotification");
-//                    imageView.setImageDrawable(icon);
-//                }
-//                catch (PackageManager.NameNotFoundException e)
-//                {
-//                    e.printStackTrace();
-//                }
-
-
-                for (ResolveInfo resolveInfo : pkgAppsList) {
-                    Log.d("DEBUG DEBUG DEBUG", "Installed package :" + resolveInfo.activityInfo.packageName);
-
-                    //int stringId = resolveInfo.activityInfo.labelRes;
-                    //Log.d("DEBUG DEBUG DEBUG", "Installed appname :" +
-                    //        (stringId==0 ?  getActivity().getApplicationInfo().nonLocalizedLabel.toString(): getActivity().getString(stringId))
-                    //);
-
-                    if(resolveInfo.activityInfo.packageName.equals("com.google.android.music")){
-                        Log.d("MUSIC", "music access");
-                        Intent intent = getContext().getPackageManager().getLaunchIntentForPackage("com.google.android.music");
-                        startActivity(intent);
-                    }
-                    //Log.d(TAG, "Source dir : " + packageInfo.sourceDir);
                 }
             }
         });
