29570d9f085482ba0122b8249e3e52479147d174
김선규 <sklationd@gmail.com>
2019-06-29 01:50:19 +0900

    state save

--- app/src/main/java/com/example/tabbed_activity/TabFragment3.java
+++ app/src/main/java/com/example/tabbed_activity/TabFragment3.java
@@ -1,6 +1,7 @@
 package com.example.tabbed_activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -19,6 +20,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -30,56 +32,75 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class TabFragment3 extends Fragment {
     private static final int SYSTEM_ALERT_WINDOW_PERMISSION = 2084;
     View view;
-    public ArrayList<String> appNameList;
+
+    ArrayList<String> appNameList;
     ArrayAdapter<String> arrayAdapter;
-    private Spinner spinner0;
+    ArrayList<String> appPackageNameList;
     List<ResolveInfo> pkgAppsList;
+    PackageManager pm;
+
+    private Spinner spinner0;
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        //Shared preferences
+        pref = getActivity().getSharedPreferences("pref", MODE_PRIVATE);
+        editor = pref.edit();
+
+        //App List
+        appNameList = new ArrayList<>();
+        appPackageNameList = new ArrayList<>();
+
+        arrayAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, appNameList);
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        pkgAppsList = getContext().getPackageManager().queryIntentActivities( mainIntent, 0);
+        pm = getContext().getPackageManager();
+
+        for (ResolveInfo resolveInfo : pkgAppsList) {
+            //package name
+            String PackageName = resolveInfo.activityInfo.packageName;
+            try {
+                String appname = pm.getApplicationLabel(pm.getApplicationInfo(resolveInfo.activityInfo.packageName, PackageManager.GET_META_DATA)).toString();
+                appNameList.add(appname);
+                appPackageNameList.add(PackageName);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_3, container, false);
 
-        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        pkgAppsList = getContext().getPackageManager().queryIntentActivities( mainIntent, 0);
-        PackageManager pm = getContext().getPackageManager();
-
-        appNameList = new ArrayList<>();
-
-        for (ResolveInfo resolveInfo : pkgAppsList) {
-            //package name
-            Log.d("DEBUG DEBUG DEBUG", "Installed package :" + resolveInfo.activityInfo.packageName);
-
-            try {
-                String appname = pm.getApplicationLabel(pm.getApplicationInfo(resolveInfo.activityInfo.packageName, PackageManager.GET_META_DATA)).toString();
-                Log.d("DEBUGDEBUGDEBUG",appname);
-                appNameList.add(appname);
-            } catch (PackageManager.NameNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-
-        arrayAdapter = new ArrayAdapter<>(getContext(),
-                android.R.layout.simple_spinner_dropdown_item,
-               appNameList);
-
         spinner0 = (Spinner) view.findViewById(R.id.spinner0);
         spinner0.setAdapter(arrayAdapter);
-
         spinner0.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getContext(),appNameList.get(i)+"가 선택되었습니다.",
-                        Toast.LENGTH_SHORT).show();
+                editor.putString("1", appPackageNameList.get(i));
+                editor.putInt("1_pos", i);
+                editor.apply();
             }
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
-
+        String prev = pref.getString("1",null);
+        if(prev != null){
+            spinner0.setSelection(pref.getInt("1_pos", -1));
+        }
 
         view.findViewById(R.id.overlay_on_button).setOnClickListener(new View.OnClickListener() {
             @Override
