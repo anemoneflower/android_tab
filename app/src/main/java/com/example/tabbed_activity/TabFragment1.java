e43a852e00eaab9cfa3a574c47c54b55422fa1d4
김선규 <sklationd@gmail.com>
2019-07-01 21:07:31 +0900

    permission before in splash activity

--- app/src/main/java/com/example/tabbed_activity/TabFragment1.java
+++ app/src/main/java/com/example/tabbed_activity/TabFragment1.java
@@ -15,7 +15,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
